4a9a6cee70842396f66f88e3f4086a157eb75cb8
schimpfen <nathanschimpf@gmail.com>
2020-04-11 19:14:58 +0100

    added RM/CM argument, test.sh options

--- test.sh
+++ test.sh
@@ -1,1 +1,19 @@
-gcc -Wall -Werror -pedantic-errors -g program.c -o program
+cmd="gcc -Wall -Werror -pedantic-errors"
+if [ "$1" != "" ]; then
+	case $1 in
+		floop )		cmd="$cmd -g -floop-interchange -o program.o"
+				;;
+		asm )		cmd="$cmd -S"
+				;;
+		asm-floop )	cmd="$cmd -S -floop-interchange"
+				;;
+		* )		echo "invalid arg"
+				exit
+	esac
+else
+	echo "no arg identified"
+fi
+
+cmd="$cmd program.c"
+echo $cmd
+$cmd
