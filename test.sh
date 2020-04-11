f3889e04b7c0dc599bfc22faa293b7f1b50586ff
schimpfen <nathanschimpf@gmail.com>
2020-04-11 19:36:23 +0100

    fixed default compile params

--- test.sh
+++ test.sh
@@ -3,15 +3,21 @@ if [ "$1" != "" ]; then
 	case $1 in
 		floop )		cmd="$cmd -g -floop-interchange -o program.o"
 				;;
+		disable )	cmd="$cmd -O0 -g -o program.o"
+				;;
 		asm )		cmd="$cmd -S"
 				;;
 		asm-floop )	cmd="$cmd -S -floop-interchange"
 				;;
-		* )		echo "invalid arg"
+		asm-disable )	cmd="$cmd -S -O0"
+				;;
+		* )		echo "default args"
+				cmd="$cmd -g -o program.o"
 				exit
 	esac
 else
-	echo "no arg identified"
+	echo "no arg identified, using default"
+	cmd="$cmd -g -o program.o"
 fi
 
 cmd="$cmd program.c"
